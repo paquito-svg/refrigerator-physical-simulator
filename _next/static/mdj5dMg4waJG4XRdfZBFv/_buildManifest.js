f4a48c79d6a1cdc42554cd9a1d7adff70a03bf3f
paquito-svg <ferfernandoaguilarcoro@gmail.com.com>
2025-11-29 23:41:38 -0400

    Updates

--- _next/static/mdj5dMg4waJG4XRdfZBFv/_buildManifest.js
+++ _next/static/mdj5dMg4waJG4XRdfZBFv/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/refrigerator-physical-simulator//_next/:path+",
+        "destination": "/refrigerator-physical-simulator/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
